e4174d642e2ccf0a59a46605180414ae9d30932b
Ayano217 <thoughtsimplification@gmail.com>
2026-07-24 20:50:33 +0600

    fix: hybrid mode for dynamic anime pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://anime-streaming-buzz.pages.dev',
-  output: 'static',
+  output: 'hybrid',
+  trailingSlash: 'ignore',
   build: {
-    format: 'directory'
+    format: 'directory',
   },
-  markdown: {
-    shikiConfig: {
-      theme: 'dracula'
-    }
-  }
 });
