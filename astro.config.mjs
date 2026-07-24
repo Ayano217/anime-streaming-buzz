94bd1552016b3f503dd49bef7ba667988e9d98df
Ayano217 <thoughtsimplification@gmail.com>
2026-07-24 21:05:36 +0600

    fix: astro config with cloudflare adapter properly loaded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://anime-streaming-buzz.pages.dev',
   output: 'hybrid',
+  adapter: cloudflare({
+    mode: 'directory',
+    functionPerRoute: false,
+  }),
   trailingSlash: 'ignore',
   build: {
     format: 'directory',
+  },
+  vite: {
+    ssr: {
+      external: ['node:buffer', 'node:path', 'node:url', 'node:fs'],
+    },
   },
 });
